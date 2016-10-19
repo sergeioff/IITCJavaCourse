ce0799717e8b71306703ec6c07093ec42f34fa36
SergeiOFF <pogorelov.s.y@gmail.com>
2016-10-19 11:31:14 +0300

    reworked with recursion

---
+++ src/exercises/exercise10/Task1.java
@@ -0,0 +1,47 @@
+package exercises.exercise10;
+
+import java.util.Scanner;
+
+/**
+ * 1. Даны два целых числа A и В (каждое в отдельной строке).
+ * Выведите все числа от A до B включительно, в порядке возрастания,
+ * если A < B, или в порядке убывания в противном случае.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        final int A = scanner.nextInt();
+        final int B = scanner.nextInt();
+
+        printNumbers(A, B);
+        printNumbersWithRecursion(A, B);
+    }
+
+    private static void printNumbers(int a, int b) {
+        if (a < b) {
+            for (int i = a; i <= b ; i++) {
+                System.out.print(i + " ");
+            }
+        } else {
+            for (int i = a; i >= b ; i--) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static void printNumbersWithRecursion(int a, int b) {
+        if (a == b) {
+            System.out.println(a);
+            return;
+        }
+        if (a < b) {
+            System.out.print(a + " ");
+            printNumbersWithRecursion(++a, b);
+        } else {
+            System.out.print(a + " ");
+            printNumbersWithRecursion(--a, b);
+        }
+    }
+}
